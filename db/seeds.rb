8f6e61744c16141eea9afc2e37426c51a86f60a3
Maria Varela <leonor.varela91330@gmail.com>
2022-04-19 21:16:44 +0200

    models resto/review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+mcdo = { name: "Mcdo", address: "7 Rue du machin", phone_number:"0788990077", category: "french" }
+just_eat =  { name: "Just Eat", address: "99 rue bidule", phone_number:"0711223344", category: "belgian"}
+sushi = { name: "Sushi", address: "2 rue du poisson", phone_number:"0755667788", category: "japanese"}
+
+[ mcdo, pizza_eat, sushi ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
